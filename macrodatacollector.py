a76f358427ae7442f7427d1948dbe220ec62746d
yangzhuowu <56561919+yangzhuowu@users.noreply.github.com>
2020-02-16 16:51:33 +0000

    Update macrodatacollector.py
    Add more function - calculate real growth rate

--- macrodatacollector.py
+++ macrodatacollector.py
@@ -3,12 +3,12 @@
 A script for monitoring macro data and housing market metircs  
 """
 import sqlite3
-from pandas.io.sql import read_frame, read_sql
+from pandas.io.sql import read_sql #read_frame,
 import pandas as pd
 import numpy as np
 import datetime as dt
-from eabsdataprocessing.utils.lldateutil import datekeyDifference, datekeyRange
-sDefaultDBPath = EABSMACRO_PATH
+from utils import datekeyRange #datekeyDifference
+sDefaultDBPath = None #EABSMACRO_PATH
 from macrodataqueries import sqlquery
 
 class MacroHousingDataManager:
@@ -18,10 +18,10 @@ class MacroHousingDataManager:
     """
     def __init__(self, query, var='hpi'):
         self.query = query
+        conn = sqlite3.connect(sDefaultDBPath) #detec_type=sqlite3.PARSE_DECLTYPES
         if conn:
             self.res = read_sql(self.query%(var,), conn)
         else:
-            conn = sqlite3.connect(sDefaultDBPath, detec_type=sqlite3.PARSE_DECLTYPES)
             conn.row_factory = sqlite3.Row
             self.res = read_sql(self.query%(var,), conn) #load hpi value into data container
 
@@ -103,11 +103,54 @@ class MacroHousingDataManager:
                 tempcontainer[currentdt] = gr
         newdf = pd.DataFrame(tempcontainer.items(), columns=['datekey', 'growth_rate'])
         newdf = newdf.sort_index(by='datekey')
-        return newdf        
+        return newdf
     
+    def loadMacrodataFromDB(self, query, vn):
+        '''load macro data from database''' #TODO: revise again
+        conn = sqlite3.connect(sDefaultDBPath) #detec_type=sqlite3.PARSE_DECLTYPES
+        conn.row_factory = sqlite3.Row
+        res = read_sql(self.query%(vn,), conn)
+        return res
+        
+    def calculateRealPriceGrowth(self, dictPerf):
+        '''Real House Price Growth Rate'''
+        df = self.res
+        df_cpi = self.loadMacrodataFromDB(sqlquery, 'cpi').groupby(['country_code'])
+        for countrycode, dd in df.groupby(['country_code']):
+            cpi = df_cpi.get_group(countrycode)
+            hpi = dd.set_index(dd['datekey'])
+            cpi = cpi.set_index(cpi['datekey'])
+            real_hpi = 100 * hpi['value'] / cpi['value']
+            
+            dRealHPI = {'real_hpi': real_hpi}
+            ndf = pd.DataFrame(dRealHPI)
+            minDate = ndf.index[0]
+            maxDate = ndf.index[-1]
+            fullIndex = datekeyRange(minDate, maxDate, step=1)
+            ndf = ndf.reindex(index=fullIndex)
+            ndf = ndf.interpolate()
+            
+            ndf = ndf.sort_index(ascending=False)
+            tempcontainer = {}
+            currentdt = ndf.first_valid_index()
+            for ii in ndf.index:
+                year  = currentdt // 100
+                month = currentdt % 100
+                previousdt = 100 * (year - 1) + month
+                if previousdt in (ndf.index.tolist()):
+                    gr = 100 * ((ndf.loc[currentdt, 'real_hpi'] - ndf.loc[previousdt, 'real_hpi']) / 
+                                ndf.loc[previousdt, 'real_hpi'])
+                    tempcontainer[currentdt] = gr
+                    
+            rate_df = pd.DataFrame(tempcontainer.items(), columns=['datekey', 'growth_rate'])
+            rate_df = rate_df.sort_index(by='datekey')
+            tempdict = dict(zip(rate_df['datekey'], rate_df['growth_rate']))
+            dictPerf = [(countrycode, 'Real House Price Growth')] = tempdict
+        return dictPerf
+            
     def test(self):
-        print "Unfinished And Will Continue!"
+        print("Unfinished And Will Continue!")
     
 if __name__ == '__main__':
-    tt = MacroHousingDataManager()
-    print tt.test()
+    tt = MacroHousingDataManager(sqlquery)
+    print(tt.test())
